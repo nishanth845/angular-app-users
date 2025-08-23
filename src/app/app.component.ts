05d2316b53cd6ecfa3f4c9531d5b939e2b365e01
nishanth845 <82376753+nishanth845@users.noreply.github.com>
2025-08-23 07:41:10 +0530

    Header hided in Login Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { HeaderComponent } from './components/header/header.component';
-
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HomeComponent,HeaderComponent,RouterModule],
+  imports: [ HeaderComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Recent Angular Features';
+  showHeader = false;
+  constructor(private router : Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Hide header on login route
+        this.showHeader = event.url !== '/login';
+      }
+    });
+  }
 }
