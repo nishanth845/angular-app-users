45059edd90ee4c8c7bb6810f65930cd27bd9d298
nishanth845 <82376753+nishanth845@users.noreply.github.com>
2025-08-23 07:29:28 +0530

    Build files

--- dist/angular-app/server/main.server.mjs
+++ dist/angular-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2OFIBXH4.mjs";import"./chunk-7ZFOLAIA.mjs";import"./chunk-AWQBR7SB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-OW4T4LLK.mjs";import"./chunk-WBPK62CB.mjs";import"./chunk-VLYRQNZJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
