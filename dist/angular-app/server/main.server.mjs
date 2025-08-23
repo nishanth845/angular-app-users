186adc2acbe862b6a25a15e0b136c8fa5575364d
nishanth845 <82376753+nishanth845@users.noreply.github.com>
2025-08-23 08:46:51 +0530

    BUILD

--- dist/angular-app/server/main.server.mjs
+++ dist/angular-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-V73LME7B.mjs";import"./chunk-JJXHREGS.mjs";import"./chunk-VLYRQNZJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-7AYB7JWR.mjs";import"./chunk-JJXHREGS.mjs";import"./chunk-VLYRQNZJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,21 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { HeaderComponent } from './components/header/header.component';
 import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ HeaderComponent,RouterModule],
+  imports: [HeaderComponent, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Recent Angular Features';
-  showHeader = false;
+  showHeader = true;
   constructor(private router : Router) {
     this.router.events.subscribe(event => {
+      console.log(event);
       if (event instanceof NavigationEnd) {
         // Hide header on login route
         this.showHeader = event.url !== '/login';
