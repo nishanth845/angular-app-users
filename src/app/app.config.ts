e85e92dd8a2f784b3b427604649354bff26b52d4
Nishanth <nishanth.reddy@stellaripl.com>
2025-10-11 09:34:29 +0530

    UI Fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,8 +7,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
 import { provideServiceWorker } from '@angular/service-worker';
-import { provideStore } from '@ngrx/store';
-import { counterReducer } from './components/loan-calculator/loan-calculator.component';
+// import { provideStore } from '@ngrx/store';
+// import { counterReducer } from './components/loan-calculator/loan-calculator.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
@@ -16,6 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([authInterceptor])),     
     provideServiceWorker('ngsw-worker.js', { }),
-    provideStore({ count: counterReducer })
+    // provideStore({ count: counterReducer })
     ],
 };
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,16 +4,25 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, MatCardModule, MatButtonModule, CommonModule],
+  imports: [RouterModule, MatCardModule, MatButtonModule, CommonModule,
+    MatButtonModule,
+    MatCardModule, MatIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   items:any;
+  steps = [
+  { icon: 'assignment', title: '1. Apply Online', desc: 'Fill out a quick online form — no paperwork required.' },
+  { icon: 'verified', title: '2. Instant Approval', desc: 'Our AI checks your details and approves your loan in minutes.' },
+  { icon: 'account_balance', title: '3. Quick Disbursement', desc: 'Funds are transferred directly into your bank account instantly.' },
+  { icon: 'payments', title: '4. Easy Repayment', desc: 'Choose flexible EMI plans that suit your needs.' },
+];
   constructor(private loginService : LoginService){
 
   }
